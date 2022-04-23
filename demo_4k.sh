fcb1d843d8e98e1b1712551577e8c06e5dfd3f17
mj <aaa5683@exntu.com>
2022-04-23 17:09:24 +0900

    demo_4k container 하나로

--- demo_4k.sh
+++ demo_4k.sh
@@ -47,37 +47,26 @@ if [[ ${SETTING_FLAG} == '1' ]]; then
   if [[ ${ENTER_FLAG} == '1' ]]; then
     read ENTER
   fi
-  docker stop non_sr
-  docker stop sr
+  docker stop demo-sr
 
   echo
 
   echo
-  echo "> Create non_sr container"
+  echo "> Create container"
   if [[ ${ENTER_FLAG} == '1' ]]; then
     read ENTER
   fi
-  docker run --privileged -itd --rm --name non_sr \
+  docker run --privileged -itd --rm --name demo-sr \
     -v ${PWD}/demo/upscale.sh:/app/upscale.sh \
-    -v ${PWD}/input:/app/input \
-    -v ${PWD}/output_sr:/app/output_sr \
-    -v ${PWD}/cred.json:/app/cred.json \
-    --device=/dev/xclmgmt24065:/dev/xclmgmt24065 --device=/dev/dri/renderD128:/dev/dri/renderD128 sr-ubuntu
-
-  echo
-
-  echo
-  echo "> Create sr container"
-  if [[ ${ENTER_FLAG} == '1' ]]; then
-    read ENTER
-  fi
-  docker run --privileged -itd --rm --name sr \
     -v ${PWD}/demo/upscale_with_sr.sh:/app/upscale_with_sr.sh \
     -v ${PWD}/demo/stack.sh:/app/stack.sh \
     -v ${PWD}/input:/app/input \
     -v ${PWD}/output_sr:/app/output_sr \
     -v ${PWD}/cred.json:/app/cred.json \
     --device=/dev/xclmgmt24065:/dev/xclmgmt24065 --device=/dev/dri/renderD128:/dev/dri/renderD128 sr-ubuntu
+
+  echo
+
 fi
 
 echo
@@ -86,12 +75,12 @@ docker ps
 
 if [[ ${NON_SR_FLAG} == '1' ]]; then
   echo
-  echo "> Upscale 4k without sr on non_sr container"
+  echo "> Upscale 4k without sr"
   TIMES=$(get_times_for_4k ${INPUT_RESOL})
   if [[ ${ENTER_FLAG} == '1' ]]; then
     read ENTER
   fi
-  time docker exec -it non_sr bash /app/upscale.sh -i ${INPUT_FILE} -c:v mpsoc_vcu_h264 -c:a copy -filter_complex "scale=w=iw*${TIMES}:h=ih*${TIMES}" "${OUTPUT_DIR}/${NON_SR_OUTPUT_FILE_NAME}" -y
+  time docker exec -it demo-sr bash /app/upscale.sh -i ${INPUT_FILE} -c:v mpsoc_vcu_h264 -c:a copy -filter_complex "scale=w=iw*${TIMES}:h=ih*${TIMES}" "${OUTPUT_DIR}/${NON_SR_OUTPUT_FILE_NAME}" -y
   # real : 2m25s
 fi
 
@@ -99,12 +88,12 @@ echo
 
 if [[ ${SR_FLAG} == '1' ]]; then
   echo
-  echo "> Upscale 4k with sr on sr container"
+  echo "> Upscale 4k with sr"
   TIMES=$(get_times_for_4k ${INPUT_RESOL})
   if [[ ${ENTER_FLAG} == '1' ]]; then
     read ENTER
   fi
-  time docker exec -it sr bash /app/upscale_with_sr.sh -i ${INPUT_FILE} -c:v mpsoc_vcu_h264 -c:a copy -filter_complex "scale_startrek=w=iw*${TIMES}:h=ih*${TIMES}:fpga=alveo:c=1" "${OUTPUT_DIR}/${SR_OUTPUT_FILE_NAME}" -y
+  time docker exec -it demo-sr bash /app/upscale_with_sr.sh -i ${INPUT_FILE} -c:v mpsoc_vcu_h264 -c:a copy -filter_complex "scale_startrek=w=iw*${TIMES}:h=ih*${TIMES}:fpga=alveo:c=1" "${OUTPUT_DIR}/${SR_OUTPUT_FILE_NAME}" -y
   # real : 4m28s
 fi
 
@@ -116,5 +105,5 @@ if [[ ${STACK_FLAG} == '1' ]]; then
   if [[ ${ENTER_FLAG} == '1' ]]; then
     read ENTER
   fi
-  time docker exec -it sr /app/stack.sh -hide_banner -i "${OUTPUT_DIR}/${NON_SR_OUTPUT_FILE_NAME}" -i "${OUTPUT_DIR}/${SR_OUTPUT_FILE_NAME}" -filter_complex hstack -y "${OUTPUT_DIR}/${STACK_OUTPUT_FILE_NAME}"
+  time docker exec -it demo-sr /app/stack.sh -hide_banner -i "${OUTPUT_DIR}/${NON_SR_OUTPUT_FILE_NAME}" -i "${OUTPUT_DIR}/${SR_OUTPUT_FILE_NAME}" -filter_complex hstack -y "${OUTPUT_DIR}/${STACK_OUTPUT_FILE_NAME}"
 fi
