82d16f2ef80ac95d75fea50b36ff57da266b9776
mj <aaa5683@exntu.com>
2022-04-26 16:12:24 +0900

    add task

--- demo_transcode.sh
+++ demo_transcode.sh
@@ -66,13 +66,12 @@ if [[ ${TR_FLAG} == '1' ]]; then
 
   echo
   time docker exec -it demo ffmpeg_nou30 -hide_banner -i ${INPUT_FILE} \
-    -filter_complex '"split=4[a][b][c][d]"' \
-    -map '"[a]"' -s 1280x720 -c:v libx264 -c:a copy -r 60 -b:v 4M -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p60.mp4" \
-    -map '"[b]"' -s 1280x720 -c:v libx264 -c:a copy -r 30 -b:v 3M -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p30.mp4" \
-    -map '"[c]"' -s 848x480 -c:v libx264 -c:a copy -r 30 -b:v 2500K -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_480p30.mp4" \
-    -map '"[d]"' -s 288x160 -c:v libx264 -c:a copy -r 30 -b:v 625k -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_288p30.mp4"
+    -filter_complex "split=4[a][b][c][d]" \
+    -map "[a]" -s 1280x720 -c:v libx264 -c:a copy -r 60 -b:v 4M -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p60.mp4" \
+    -map "[b]" -s 1280x720 -c:v libx264 -c:a copy -r 30 -b:v 3M -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p30.mp4" \
+    -map "[c]" -s 848x480 -c:v libx264 -c:a copy -r 30 -b:v 2500K -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_480p30.mp4" \
+    -map "[d]" -s 288x160 -c:v libx264 -c:a copy -r 30 -b:v 625k -y "${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_288p30.mp4"
 
   echo
 
-  ls -alht "${OUTPUT_DIR}"
 fi
