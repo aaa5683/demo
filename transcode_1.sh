364e3959dac8873bcbfb4c4520e702d866cb3f0b
mj <aaa5683@exntu.com>
2022-04-28 11:00:15 +0900

    cmd 재설정

--- transcode_1.sh
+++ transcode_1.sh
@@ -8,16 +8,22 @@ OUTPUT_FILE_PREFIX_NAME=$3
 #-vf 'scale=3840x2160:flags=lanczos' \
 #-c:v libx264 -c:a copy -r 60 -b:v 1M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k.mp4"
 
+#FFMPEG_ARGS="-i ${INPUT_FILE} \
+#-filter_complex 'split=4[a][b][c][d]; \
+#[a]scale=1280x720:flags=lanczos[aa]; \
+#[b]scale=1920x1080:flags=lanczos[bb]; \
+#[c]scale=2560x1440:flags=lanczos[cc]; \
+#[d]scale=3840x2160:flags=lanczos[dd]' \
+#-map '[aa]' -c:v libx264 -c:a copy -b:v 2.5M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p30.mp4 \
+#-map '[bb]' -c:v libx264 -c:a copy -b:v 4M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1080p30.mp4 \
+#-map '[cc]' -c:v libx264 -c:a copy -b:v 8M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1440p30.mp4 \
+#-map '[dd]' -c:v libx264 -c:a copy -b:v 10M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k30.mp4"
+
 FFMPEG_ARGS="-i ${INPUT_FILE} \
--filter_complex 'split=4[a][b][c][d]; \
-[a]scale=1280x720:flags=lanczos[aa]; \
-[b]scale=1920x1080:flags=lanczos[bb]; \
-[c]scale=2560x1440:flags=lanczos[cc]; \
-[d]scale=3840x2160:flags=lanczos[dd]' \
--map '[aa]' -c:v libx264 -c:a copy -b:v 2.5M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p30.mp4 \
--map '[bb]' -c:v libx264 -c:a copy -b:v 4M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1080p30.mp4 \
--map '[cc]' -c:v libx264 -c:a copy -b:v 8M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1440p30.mp4 \
--map '[dd]' -c:v libx264 -c:a copy -b:v 10M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k30.mp4"
+-s 1280x720 -c:v libx264 -c:a copy -b:v 2.5M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_720p30.mp4 \
+-s 1920x1080 -c:v libx264 -c:a copy -b:v 4M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1080p30.mp4 \
+-s 2560x1440 -c:v libx264 -c:a copy -b:v 8M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_1440p30.mp4 \
+-s 3840x2160 -c:v libx264 -c:a copy -b:v 10M -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_4k30.mp4"
 
 cmd="time ffmpeg_nou30 -hide_banner ${FFMPEG_ARGS}"
 
--- transcode_u30_1.sh
+++ transcode_u30_1.sh
@@ -27,11 +27,11 @@ FFMPEG_ARGS="-c:v mpsoc_vcu_h264 -i ${INPUT_FILE} \
 out_1_width=1280: out_1_height=720: out_1_rate=full: \
 out_2_width=1920: out_2_height=1080: out_2_rate=full: \
 out_3_width=2560: out_3_height=1440: out_3_rate=full: \
-out_4_width=3840: out_4_height=2160: out_4_rate=full [a][b][c][d]; [0:1]asplit=outputs=4[aud1][aud2][aud3][aud4]' \
--map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -map '[aud1]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30.mp4 \
--map '[b]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -map '[aud2]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p30.mp4 \
--map '[c]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -map '[aud3]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p30.mp4 \
--map '[d]' -cores 4 -b:v 10M -c:v mpsoc_vcu_h264 -map '[aud4]' -c:a aac -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k30.mp4"
+out_4_width=3840: out_4_height=2160: out_4_rate=full [a][b][c][d];' \
+-map '[a]' -cores 4 -b:v 2.5M -c:v mpsoc_vcu_h264 -c:a copy -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_720p30.mp4 \
+-map '[b]' -cores 4 -b:v 4M -c:v mpsoc_vcu_h264 -c:a copy -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1080p30.mp4 \
+-map '[c]' -cores 4 -b:v 8M -c:v mpsoc_vcu_h264 -c:a copy -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_1440p30.mp4 \
+-map '[d]' -cores 4 -b:v 10M -c:v mpsoc_vcu_h264 -c:a copy -y ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX_NAME}_u30_4k30.mp4"
 
 cmd="time ffmpeg -hide_banner ${FFMPEG_ARGS}"
 
